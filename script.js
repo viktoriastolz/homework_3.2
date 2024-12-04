ef138f7aefb81c075ca67b967a79b9108b8eae99
Viktoria Stolz <88290448+viktoriastolz@users.noreply.github.com>
2024-12-04 17:53:59 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+const string1 = prompt("Enter your name:");
+const string2 = prompt("Enter your surname:");
+const string3 = prompt("Enter your age:");
+
+alert(`${string3}, ${string1}, ${string2}`);
